db3e9b9fa38f9e8a8aa2358a6bd9fb2e258857aa
W2PAW <juser@chalkbox>
2015-09-02 20:40:12 -0400

    pull legacy code

--- palm-foto-convert.sh
+++ palm-foto-convert.sh
@@ -52,7 +52,7 @@ hashOnly=$(echo -n ${hash:0:40}) # extract first 40 char
 
   # test if only [0-9abcdef]
 
-grep -q "${hashOnly}" "${workHashes}" ; x=$? # 0 if match, 1 otherwise
+grep -i -q "${hashOnly}" "${workHashes}" ; x=$? # 0 if match, 1 otherwise 
 if [ $x = 0 ] ; then 
   exit 0  #exit, we can skip this one
 fi       
@@ -90,101 +90,3 @@ rm "${localCopy}"
 
 # exit w/o error
 exit 0  #nothing below should be run
-
-#-----------------------------
-# palm-foto-convert.sh - convert synced photo pdb into jpgs, make sure they're converted only once,
-#			 and add exif data for an easy import into shotwell.
-
-########
-##  Setup 
-########
-
-fotoSource="/home/juser/jpilot/last-sync/"
-fotoTarget="/home/juser/jpilot/extracted-photos/test2"
-foto1sha="/home/juser/jpilot/extracted-photos/test2/foto1sha.txt"
-
-check_for_existing_hash () {
-    # "0" means a match,  "1" means no match, "$1" is the hash
-    hash=$(tr 'ABCDEF' 'abcdef' <<<$1) #convert to lower case
-    grep "$1" ${foto1sha} &>/dev/null #only look at exit status
-    return $?
-}
-
-add_new_hash () {
-    # add a new hash and filename to the list of already processed foto files
-    # "$1" is hash, "$2" is filename. Example line:
-    # bb294f2afb7458e32afbbfd5bfe2292f09396486,  "Photo_090910_001.jpg.pdb"
-    echo "\"\""
-
-}
-
-
-
-########
-# Main
-########
-
-#find all files in ${fotoSource}; sha1sum and check against sums in ${foto1sha}
-# if new,
-#	1. add hash to ${foto1sha}
-#	2. convert to jpg, put in $fotoTarget
-#	3. use `touch` to re-set timestamp
-#	3. use exiftool to add EXIF data so that shotwell sorts via date when it imports 
-
-for x in $(find ${fotoSource} -maxdepth 1 -type f -name "*.jpg.pdb") ; do  #find files
-	sum=$(sha1sum $x |cut -b-40 ) #get sha1sum only
-	grep ${sum} ${foto1sha} &>/dev/null #look to see if new or not, only look at exit status.
-	if [ "$?" -eq "0" ] ; then #then we have new photo
-		y="${x##/*/}" #remove "tree" from filename
-		z="${y%.pdb}" #remove .pdb; $z is now perdicted filename of converted photo
-		echo "${sum},\"${x}\"">>${foto1sha}	#add sha1sum to database
-		pilot-foto -c ${x}			#convert to jpg, will stay in same dir
-		dupeCount="0" ; targetFilename="${z}"   #setup for target filename test
-	        while [ -e "${fotoTarget}${targetFilename}" ] ; do
-			((dupeCount++))
-			targetFilename=${z%.jpg}-${dupeCount}.jpg"
-		#mv "${fotoSource}/${fotoName}"  #move (should add code to look for colosions)
-	       #touch
-       		#exiftool #timestamp	       
-
-
-
-#create tmp dir  #future md5sum?
-#mkdir /tmp/.photo-${uq}
-#cd /tmp/.photo-${uq}
-
-cd ${fotoTarget} #change to target directiry
-
-
-for x in $(find ${fotoSource} -name "*jpg.pdb") ; do
-	#check for name clobber 
-		# /home/juser/jpilot/last-sync/Photo_043010_001.jpg.pdb
-		# becomes "Photo_043010_001.jpg"
-	y=$(echo "$x" |sed -e 's_^/home/juser/jpilot/last-sync/__' -e 's_.pdb$__')
-	# if file exists at target then 
-	if [ -e "$y" ] ; then
-		echo "not overwriting ${y}" #error message
-		true #do nothing
-	else #else convert to normal jpg
-		pilot-foto -c ${x} #convert, store in current dir
-
-	fi 
-
-
-done
-
-
-#target="for x in $(find ${startD} -type f -name "*.jpg.pdb") ; do 
-
-########
-# cleanup
-########
-exit
-
-
-{
-  "_filename_palm": "t2z1vH4.mp4",
-  "sha1sum": 0,
-  "annotations": null,
-  }
-
